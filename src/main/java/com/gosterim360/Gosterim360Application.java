ce942a349f1352b5cc46b063db7998ee88a3c28e
orhan <orhantrkmn749@gmail.com>
2025-07-08 10:37:41 +0300

    feat: initialize project with Gosterim360 base structure

---
+++ src/main/java/com/gosterim360/Gosterim360Application.java
@@ -0,0 +1,13 @@
+package com.gosterim360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gosterim360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gosterim360Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gosterim360/Gosterim360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gosterim360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gosterim360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
