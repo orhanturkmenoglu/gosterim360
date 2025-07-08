d174371490a3f3ad4903412ea85717348b169fbb
orhan <orhantrkmn749@gmail.com>
2025-07-08 12:36:30 +0300

    feat (controller): added MovieController with create, read, update, delete operations

--- src/main/java/com/gosterim360/controller/MovieController.java
+++ src/main/java/com/gosterim360/controller/MovieController.java
@@ -1,9 +1,50 @@
 package com.gosterim360.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.gosterim360.dto.request.MovieRequestDTO;
+import com.gosterim360.dto.response.MovieResponseDTO;
+import com.gosterim360.service.MovieService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/movies")
+@RequiredArgsConstructor
 public class MovieController {
+
+    private final MovieService movieService;
+
+    @PostMapping
+    public ResponseEntity<MovieResponseDTO> createMovie(@Valid @RequestBody MovieRequestDTO movieRequestDTO) {
+        MovieResponseDTO movie = movieService.createMovie(movieRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(movie);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MovieResponseDTO>> getAllMovies() {
+        return ResponseEntity.ok(movieService.getAllMovies());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MovieResponseDTO> getMovieById(@PathVariable UUID id) {
+        return ResponseEntity.ok(movieService.getMovieById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MovieResponseDTO> updateMovie(@PathVariable UUID id,
+                                                        @Valid @RequestBody MovieRequestDTO movieRequestDTO) {
+        MovieResponseDTO updatedMovie = movieService.updateMovie(id, movieRequestDTO);
+        return ResponseEntity.ok(updatedMovie);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMovieById(@PathVariable UUID id) {
+        movieService.deleteMovieById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
