99bacbcb8fe74dd202b774d00438a50e3197934f
orhan <orhantrkmn749@gmail.com>
2025-07-08 12:37:21 +0300

    feat: add validation annotations to MovieRequestDTO

--- src/main/java/com/gosterim360/dto/request/MovieRequestDTO.java
+++ src/main/java/com/gosterim360/dto/request/MovieRequestDTO.java
@@ -1,5 +1,8 @@
 package com.gosterim360.dto.request;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,17 +12,26 @@ import lombok.Setter;
 @Builder
 public class MovieRequestDTO {
 
+    @NotEmpty(message = "Name is required")
     private String name;
 
+    @NotEmpty(message = "Description is required")
     private String description;
 
+    @NotEmpty(message = "Genre is required")
     private String genre;
 
+    @NotNull(message = "Director is required")
+    @Positive(message = "Director must be a positive number")
     private Double duration;
 
+    @NotNull(message = "Rating is required")
+    @Positive(message = "Rating must be a positive number")
     private Double rating;
 
+    @NotNull(message = "Review count is required")
     private Integer reviewCount;
 
+    @NotEmpty(message = "Poster URL is required")
     private String posterUrl;
 }
