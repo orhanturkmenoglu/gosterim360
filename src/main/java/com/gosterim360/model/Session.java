31848b088794db85753609835069f89282afa2ed
orhan <orhantrkmn749@gmail.com>
2025-07-21 22:49:16 +0300

    feat

--- src/main/java/com/gosterim360/model/Session.java
+++ src/main/java/com/gosterim360/model/Session.java
@@ -4,7 +4,6 @@ import com.gosterim360.common.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
